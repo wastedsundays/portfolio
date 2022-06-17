748d2aa8c929b2425c8ff49d8e6207381b914b4e
Adam Hauck <ad.hauck@gmail.com>
2022-06-17 01:42:29 -0700

    added sections to main page

---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+const Loading = () => {
+  return (
+    <img src="spinngin-record.gif" alt="Loading" className="loading" id="loading" />
+  )
+}
+
+export default Loading
---
+++ src/pages/MainPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import HomeSection from "../components/HomeSection";
+import AboutSection from "../components/AboutSection";
+import ProjectsSection from '../components/ProjectsSection';
+import ContactSection from '../components/ContactSection';
+
+function MainPage() {
+  return (
+    <div>
+        <HomeSection />
+        <AboutSection />
+        <ProjectsSection />
+        <ContactSection />
+    </div>
+  )
+}
+
+export default MainPage
