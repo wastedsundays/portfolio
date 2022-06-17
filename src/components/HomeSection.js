72625490e2a21ba6a704a6ab62dddd4e69ec91c2
Adam Hauck <ad.hauck@gmail.com>
2022-06-17 00:30:01 -0700

    Home section basic content

---
+++ src/components/HomeSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomeSection() {
+  return (
+    <section className='home-section' id='home'>
+        <h1>Adam H</h1>
+        <h2>Web Designer and Developer</h2>
+        <p>Ipsume Dipsum Tripsum</p>
+    </section>
+  )
+}
+
+export default HomeSection
