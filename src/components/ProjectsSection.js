1c220fca5a296863ab76a7920e61e445133b3d60
Adam Hauck <ad.hauck@gmail.com>
2022-06-17 00:34:49 -0700

    projects section basic setup

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProjectsSection() {
+  return (
+    <section className='projects-section' id='projects'>
+        <h1>Projects</h1>
+        <p>Ipsume Dipsum Tripsum</p>
+    </section>
+  )
+}
+
+export default ProjectsSection
