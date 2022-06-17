a8660fd4a609910c8f1740ca1521d6f84b641f0e
Adam Hauck <ad.hauck@gmail.com>
2022-06-17 00:33:29 -0700

    about section component setup

---
+++ src/components/AboutSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutSection() {
+  return (
+    <section className='about-section' id='about'>
+        <h1>About Me</h1>
+        <p>Ipsume Dipsum Tripsum</p>
+    </section>
+  )
+}
+
+export default AboutSection
