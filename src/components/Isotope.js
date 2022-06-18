514e762d540e58490f0332b249c15e2c19cd7a24
Adam Hauck <ad.hauck@gmail.com>
2022-06-17 20:47:32 -0700

    hard-coded isotope data (for now)

--- src/components/Isotope.js
+++ src/components/Isotope.js
@@ -1,6 +1,25 @@
 import * as React from 'react';
 import Isotope from 'isotope-layout';
 
+import crystal from '../skills/skill-icon-crystalreports.png';
+import excel from '../skills/skill-icon-excel.png';
+import git from '../skills/skill-icon-git.png';
+import gulp from '../skills/skill-icon-gulp.png';
+import HTML from '../skills/skill-icon-html.png';
+import illustrator from '../skills/skill-icon-illustrator.png';
+import javascript from '../skills/skill-icon-javascript.png';
+import jetreports from '../skills/skill-icon-jetreports.png';
+import jquery from '../skills/skill-icon-jquery.png';
+import photoshop from '../skills/skill-icon-photoshop.png';
+import php from '../skills/skill-icon-php.png';
+import sass from '../skills/skill-icon-sass.png';
+import shopify from '../skills/skill-icon-shopify.png';
+import sql from '../skills/skill-icon-sql.png';
+import vscode from '../skills/skill-icon-vscode.png';
+import woocommerce from '../skills/skill-icon-woocommerce.png';
+import wordpress from '../skills/skill-icon-wordpress.png';
+import xd from '../skills/skill-icon-xd.png';
+
 
 
 
@@ -33,25 +52,71 @@ const IsotopeReact = () => {
     return (
       <>
         <ul>
-          <li onClick={handleFilterKeyChange('*')}>Show Both</li>
-          <li onClick={handleFilterKeyChange('vege')}>Show Veges</li>
-          <li onClick={handleFilterKeyChange('fruit')}>Show Fruits</li>
-          <li onClick={handleFilterKeyChange('dev')}>Show Dev Tools</li>
+          <li onClick={handleFilterKeyChange('*')}>All</li>
+          <li onClick={handleFilterKeyChange('dev')}>Dev</li>
+          <li onClick={handleFilterKeyChange('design')}>Design</li>
+          <li onClick={handleFilterKeyChange('other')}>Other</li>
         </ul>
         <hr />
         <ul className="filter-container">
-          <div className="filter-item vege">
-            <span>Cucumber</span>
+          <div className="filter-item other">
+          <img src={crystal} alt="Crystal Reports"/>
           </div>
-          <div className="filter-item fruit">
-            <span>Apple</span>
+          <div className="filter-item dev">
+          <img src={jetreports} alt="CSS"/>
           </div>
-          <div className="filter-item fruit">
-            <span>Orange</span>
+          <div className="filter-item other">
+          <img src={excel} alt="Excel"/>
           </div>
-          <div className="filter-item fruit vege">
-            <span>Tomato</span>
+          <div className="filter-item dev">
+          <img src={git} alt="Git"/>
           </div>
+          <div className="filter-item dev">
+          <img src={gulp} alt="Gulp"/>
+          </div>
+          <div className="filter-item dev">
+            <img src={HTML} alt="html"/>
+          </div>
+          <div className="filter-item design">
+          <img src={illustrator} alt="Illustrator"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={javascript} alt="JavaScript"/>
+          </div>
+          <div className="filter-item other">
+          <img src={jetreports} alt="Jet Reports"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={jquery} alt="jQuery"/>
+          </div>
+          <div className="filter-item design">
+          <img src={photoshop} alt="Photoshop"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={php} alt="PHP"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={sass} alt="SASS"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={shopify} alt="Shopify"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={sql} alt="SQL"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={vscode} alt="VS Code"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={woocommerce} alt="WooCommerce"/>
+          </div>
+          <div className="filter-item dev">
+          <img src={wordpress} alt="WordPress"/>
+          </div>
+          <div className="filter-item design">
+          <img src={xd} alt="XD"/>
+          </div>
+
         </ul>
       </>
     )
