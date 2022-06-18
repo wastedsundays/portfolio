140e8610a5fc84f3cb501a12ff1954c20f52dc6b
Adam Hauck <ad.hauck@gmail.com>
2022-06-17 22:21:58 -0700

    sidebar visibility on mobile

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 import { useState, useEffect } from 'react'
@@ -23,7 +22,6 @@ function TabPanel(props) {
       >
         {value === index && (
           <Box sx={{ p: 4 }}>
-            {/* <Typography>{children}</Typography> */}
             {children}
           </Box>
         )}
