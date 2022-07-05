8d130c8f68c3a41385acae1590c66fbeb2c09eeb
Adam Hauck <ad.hauck@gmail.com>
2022-07-05 13:18:35 -0700

    added react-helmet and proper page titles

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import HeaderBar from '../components/HeaderBar';
@@ -40,7 +41,13 @@ function ProjectPage() {
     <>
     <HeaderBar />
     { isLoaded ?
+
       <div className='project-home-section'>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>{restData[0].title.rendered} by Adam H.</title>
+            <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <img
           src={restData[0].acf.project_featured_image}
           className="featured-image"
