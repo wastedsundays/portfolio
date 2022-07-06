f3b4b8545f7a9dcc3bfcef27c77098b57965afd0
Adam Hauck <ad.hauck@gmail.com>
2022-07-06 13:54:48 -0700

    added 404 page and image

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import  Record  from '../images/404record.gif';
 
 function Page404() {
   return (
     <div className="error-message">
       <h1>:(</h1>
       <p>You look lost...</p>
+      <div> 
+        <img
+                src={Record}
+                className="nav-logo"
+                alt="Logo"
+                />
+      </div>
       <NavLink to={"/"}>
         <button className="link-button">Home</button>
       </NavLink>
